13ad4e9002a6147c6733d367752b51a1791e3992
Chise1 <chise123@live.com>
2021-05-16 18:08:15 +0800

    for relational.py add extra describe args.

--- tortoise/fields/base.py
+++ tortoise/fields/base.py
@@ -300,7 +300,7 @@ class Field(metaclass=_FieldMeta):
                     "default":      ...     # The default value (coerced to int/float/str/bool/null)
                     "description":  str     # Description of the field (nullable)
                     "docstring":    str     # Field docstring (nullable)
-                    "generated_sql": dict    # Field generated_sql info
+                    "generated_sql": dict   # Field generated_sql info
                 }
 
             When ``serializable=False`` is specified some fields are not coerced to valid
@@ -356,10 +356,11 @@ class Field(metaclass=_FieldMeta):
             "description": self.description,
             "docstring": self.docstring,
             "constraints": self.constraints,
-            "generated_sql": {"mysql":self.get_for_dialect("mysql", "GENERATED_SQL"),
-                              "sqlite":self.get_for_dialect("sqlite", "GENERATED_SQL"),
-                              "asyncpg":self.get_for_dialect("asyncpg", "GENERATED_SQL"),
-                              }
+            "generated_sql": {
+                "mysql": self.get_for_dialect("mysql", "GENERATED_SQL"),
+                "sqlite": self.get_for_dialect("sqlite", "GENERATED_SQL"),
+                "asyncpg": self.get_for_dialect("asyncpg", "GENERATED_SQL"),
+            },
         }
 
         if self.has_db_field:
