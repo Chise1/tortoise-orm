549b4e68d27359c6ed04be6625c949734082284c
Chise1 <chise123@live.com>
2021-05-20 15:15:45 +0800

    修复bug

--- tortoise/contrib/pydantic/creator.py
+++ tortoise/contrib/pydantic/creator.py
@@ -312,7 +312,7 @@ def pydantic_model_creator(
                     computed=tuple(
                         str(v[prefix_len:]) for v in computed if v.startswith(fname + ".")
                     ),
-                    name = f"{name or fqname}.{fqname}",
+                    name = f"{name or fqname}.{fname}",
                     stack=new_stack,
                     allow_cycles=_allow_cycles,
                     sort_alphabetically=sort_alphabetically,
